708b31712eb69477c023983abd4a8b287a95896e
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-06 15:44:33 -0700

    basic files & folders for game

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  console.log( "ready!" );
+  //   // console.log( "button.click() called." );
+});   // this is for all document
+// all code to manipulate the DOM
+// goes inside this function
