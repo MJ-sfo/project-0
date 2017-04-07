2b864218021ed80caaccc71d5e1128dc3dcacddb
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-07 15:35:30 -0700

    has board of right size. syling no longer in html

--- scripts/app.js
+++ scripts/app.js
@@ -9,30 +9,26 @@ $( document ).ready(function() {
 // jsboard.piece({ text: pieceName [,cssProperties] });
 setInterval(movePlane, 20);
 var keys = {}
-
 $(document).keydown(function(e) {
    keys[e.keyCode] = true;
 });
-
 $(document).keyup(function(e) {
    delete keys[e.keyCode];
 });
-
-
 function movePlane() {
    for (var direction in keys) {
        if (!keys.hasOwnProperty(direction)) continue;
        if (direction == 37) {
-           $("#plane").animate({left: "-=5"}, 0);
+           $("#alien").animate({left: "-=5"}, 0);
        }
        if (direction == 38) {
-           $("#plane").animate({top: "-=5"}, 0);
+           $("#alien").animate({top: "-=5"}, 0);
        }
        if (direction == 39) {
-           $("#plane").animate({left: "+=5"}, 0);
+           $("#alien").animate({left: "+=5"}, 0);
        }
        if (direction == 40) {
-           $("#plane").animate({top: "+=5"}, 0);
+           $("#alien").animate({top: "+=5"}, 0);
        }
    }
-}  
+}
