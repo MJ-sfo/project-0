54d3eb1e05a98062cdfcbe00c016719673badbdb
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-06 23:04:31 -0700

    8x8 table grid visible

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,6 @@ $( document ).ready(function() {
 });   // this is for all document
 // all code to manipulate the DOM
 // goes inside this function
+
+// jsboard.board({ attach: 'game', size: 12x12, style: "checkerboard" });
+// jsboard.piece({ text: pieceName [,cssProperties] });
