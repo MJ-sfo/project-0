f9006ce55187b59e5771b8fd4087dd3ed4df19fd
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-07 13:55:10 -0700

    rocket moves with arrow keys

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,32 @@ $( document ).ready(function() {
 
 // jsboard.board({ attach: 'game', size: 12x12, style: "checkerboard" });
 // jsboard.piece({ text: pieceName [,cssProperties] });
+setInterval(movePlane, 20);
+var keys = {}
+
+$(document).keydown(function(e) {
+   keys[e.keyCode] = true;
+});
+
+$(document).keyup(function(e) {
+   delete keys[e.keyCode];
+});
+
+
+function movePlane() {
+   for (var direction in keys) {
+       if (!keys.hasOwnProperty(direction)) continue;
+       if (direction == 37) {
+           $("#plane").animate({left: "-=5"}, 0);
+       }
+       if (direction == 38) {
+           $("#plane").animate({top: "-=5"}, 0);
+       }
+       if (direction == 39) {
+           $("#plane").animate({left: "+=5"}, 0);
+       }
+       if (direction == 40) {
+           $("#plane").animate({top: "+=5"}, 0);
+       }
+   }
+}  
