52d442e5d4b91b75c7d43ae7edf2e157fdd15c50
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-09 19:34:23 -0700

    lunar background, standard var alien = .offset(); for movement

--- scripts/app.js
+++ scripts/app.js
@@ -20,45 +20,49 @@ $(document).keyup(function(e) {
    delete keys[e.keyCode];
 });  // $(document).keyup
 function movePlane() {
-  var strL = $("#alien").animate()[0];
+  // var strL = $("#alien").animate()[0];
+  var alien = $('#alien').offset();
    for (var direction in keys) {
        if (!keys.hasOwnProperty(direction)) continue;
-       if (direction == 37 && !borderL) {
-           $("#alien").animate({left: "-=5"}, 0);
-            console.log(strL.x);
-       }
-       if (direction == 38 && !borderT) {
-           $("#alien").animate({top: "-=5"}, 0);
-            console.log(strL.y);
-       }
-       if (direction == 39 && !borderR) {
-           $("#alien").animate({left: "+=5"}, 0);
-          //  console.log($("#alien").animate({left: "+=5"}, 0));
-           console.log(strL.x);
-       }
-       if (direction == 40 && !borderB) {
-           $("#alien").animate({top: "+=5"}, 0);
-            console.log(strL.y);
-       }
-   }
-}  // function movePlane
+         if (direction == 37 && !borderL) {
+             $("#alien").animate({left: "-=5"}, 0);
+              // borderR = false;
+              console.log(alien.left);
+         }
+         if (direction == 38 && !borderT) {
+             $("#alien").animate({top: "-=5"}, 0);
+              console.log(alien.top);
+         }
+         if (direction == 39 && !borderR) {
+             $("#alien").animate({left: "+=5"}, 0);
+            //  console.log($("#alien").animate({left: "+=5"}, 0));
+            //  console.log(strL.x);
+              console.log(alien.left);
+                borderL = false;
+         }
+         if (direction == 40 && !borderB) {
+             $("#alien").animate({top: "+=5"}, 0);
+              console.log(alien.top);
+         }
+   }   // for (var direction in keys
+}  //     function movePlane
 
 $(window).keydown(function() {
         var alien = $('#alien').offset();
         if (alien.left >= 780 ) {
-        console.log('hit right border')
+        console.log('hit right border');
           borderR = true;
         }
         else if (alien.left <= 0 ) {
-        console.log('hit left border')
+        console.log('hit left border');
           borderL = true;
         }
         else if (alien.top <= 0 ) {
-        console.log('hit top border')
+        console.log('hit top border');
           borderT = true;
         }
         else if (alien.top >= 300 ) {
-        console.log('hit bottom border')
+        console.log('hit bottom border');
           borderB = true;
         }
     }) //    $(window).keydown
